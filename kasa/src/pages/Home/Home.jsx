d6a5dcf527c34797a1c673cec9295dbb492528de
liujiaodaliao <mengmeijia@126.com>
2023-12-29 17:48:47 +0100

    Start adding homepage jsx

--- kasa/src/pages/Home/Home.jsx
+++ kasa/src/pages/Home/Home.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import Banner from "../../components/Banner/Banner" 
+import "./Home.css"
+// import Banner 
+//import Cards
+
 
 function Home() {
-  return <div>Home Page</div>;
+  return (
+  <section className="homecontainer">
+    <Banner />
+  </section>
+  );
 }
 
 export default Home;
