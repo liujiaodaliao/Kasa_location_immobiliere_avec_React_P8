7588247a204c0dd5122bb83e36e4d5aad029a8e9
liujiaodaliao <mengmeijia@126.com>
2024-01-12 03:52:45 +0100

    add component Tag

---
+++ kasa/src/components/Tag/Tag.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Tag.css"
+
+function Tag(props) {
+  return (
+    <div className="tagContainer">
+      <span className="tagButton">{props.tag}</span>
+    </div>
+  );
+}
+
+export default Tag
--- kasa/src/pages/Location/Location.jsx
+++ kasa/src/pages/Location/Location.jsx
@@ -3,7 +3,8 @@ import { useState, useEffect } from "react";
 
 
 import Collapse from "../../components/Collapse/Collapse";
-import moduleName from '../../components/Host/Host'
+import Host from '../../components/Host/Host';
+import Tag from '../../components/Tag/Tag';
 import "./Location.css"
 
 // carrousel-轮播图 collapse-折叠组件 host-主机信息 rate-评分 tag-标签 axios-获取图片数据
