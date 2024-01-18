1add3789c382c1c9011c54b4cac71b12ae4c1406
liujiaodaliao <mengmeijia@126.com>
2024-01-18 03:01:58 +0100

    Add component location and optimize page layout

--- kasa/src/pages/Location/Location.jsx
+++ kasa/src/pages/Location/Location.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 import Carrousel from "../../components/Carrousel/Carrousel";
@@ -12,7 +13,76 @@ import "./Location.css"
 // carrousel-轮播图 collapse-折叠组件 host-主机信息 rate-评分 tag-标签 axios-获取图片数据
 
 function Location() {
-  return <div>Location Page</div>;
+  // 从 React Router 获取路由参数和导航函数
+  const params = useParams();
+  const navigate = useNavigate();
+
+  // 使用状态钩子和副作用钩子来获取数据
+  const [pickedAppart, setPickedAppart] = useState();
+	useEffect(() => {
+		const getData = async () => {
+			const res = await axios.get("/location.json"); 
+      //fetch api avec AXIOS 
+			const picked = res.data.find(({ id }) => id === params.id);
+			res.data.map(() => setPickedAppart(picked));
+			if (picked === undefined) {
+				navigate("/404", { state: { message: "Can't get data" } }); 
+        //renvoi vers la page 404 en cas d'URL de logement invalide
+			}
+		};
+		getData();
+		// eslint-disable-next-line
+	}, []); // array vide du useEffect pour ne lancer qu'une seule fois
+
+  const slidePics = pickedAppart && pickedAppart.pictures;
+	const tags = pickedAppart && pickedAppart.tags;
+	const equipments = pickedAppart && pickedAppart.equipments;
+	const equip =
+		pickedAppart &&
+		equipments.map((item, index) => (
+			<li key={index} className="equipList">
+				{item}
+			</li>
+		));
+
+  return (
+		pickedAppart && (
+			<div key={params.id} className="fiche-container">
+				<Carrousel slides={slidePics} />
+				<section className="hostInfo-container">
+					<div className="title-tags-container">
+						<div className="title-container redFont">
+							<h1>{pickedAppart.title}</h1>
+							<h3>{pickedAppart.location}</h3>
+						</div>
+						<div className="tags-container">
+							{tags.map((tag) => (
+								<Tag key={tag} tag={tag} />
+							))}
+						</div>
+					</div>
+					<div className="rate-host-container">
+						<div className="host-container redFont">
+							<Host
+								hostName={pickedAppart.host.name}
+								hostPic={pickedAppart.host.picture}
+							/>
+						</div>
+						<div className="rate-container">
+							<Rate score={pickedAppart.rating} />
+						</div>
+					</div>
+				</section>
+				<div className="collapse-fiche-container">
+					<Collapse
+						aboutTitle="Description"
+						aboutText={pickedAppart.description}
+					/>
+					<Collapse aboutTitle="Équipements" aboutText={equip} />
+				</div>
+			</div>
+		)
+	);
 }
 
 export default Location;
